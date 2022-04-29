b2fdf3ed0b06994c6f43bdff035ebf25051aeef0
Li Yizhe <yizhe.li@ncs.com.sg>
2022-04-29 08:44:44 +0800

    1. initialized event list page.

--- src/app/views/event/event-list/event-list.component.ts
+++ src/app/views/event/event-list/event-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
+import * as constants from '../../../classes/constants';
+import * as moment from 'moment';
+import { Event } from '../../../classes/event';
+import { EventService } from '../event.service';
 
 @Component({
   selector: 'app-event-list',
@@ -6,10 +12,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-list.component.scss']
 })
 export class EventListComponent implements OnInit {
+  testUserID: string = "20220420000000000000";
+  resultEventList: Event[];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private eventService : EventService) { }
 
   ngOnInit(): void {
+    let queryParams = new HttpParams();
+    queryParams = queryParams.append("customerID", this.testUserID);
+
+    this.eventService.getEventListByParam(queryParams).then((data) => {
+      this.resultEventList = data;
+      this.resultEventList.sort((a,b) => 
+        (moment(a.startTime, constants.DATETIME_FORMAT).toDate() < moment(b.startTime, constants.DATETIME_FORMAT).toDate()) ? 1 :
+          ((moment(a.startTime, constants.DATETIME_FORMAT).toDate() > moment(b.startTime, constants.DATETIME_FORMAT).toDate()) ? -1 : 0));
+    });
   }
 
+  
 }
