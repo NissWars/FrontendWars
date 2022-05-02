a678f95d8e81b205a49669362342f61c58825386
kerri <kerri.cheng1@ncs.com.sg>
2022-05-02 16:06:41 +0800

    hid header from Login page

--- src/app/containers/default-layout/default-header/default-header.component.ts
+++ src/app/containers/default-layout/default-header/default-header.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Params, Router } from '@angular/router';
+import { Params, Router, NavigationStart } from '@angular/router';
 
 import { ClassToggleService, HeaderComponent } from '@coreui/angular';
 
@@ -12,6 +12,8 @@ export class DefaultHeaderComponent extends HeaderComponent {
 
   @Input() sidebarId: string = "sidebar";
 
+  displayHeader: boolean = false; //hide header for Login page
+
   public newMessages = new Array(4)
   public newTasks = new Array(5)
   public newNotifications = new Array(5)
@@ -21,6 +23,21 @@ export class DefaultHeaderComponent extends HeaderComponent {
   constructor(private classToggler: ClassToggleService,private route: Router) {
     super();
     console.log(sessionStorage.getItem('custID'));
+
+
+    //hide header for Login page  
+    route.events.forEach((event) => { 
+      if (event instanceof NavigationStart) {
+        if (event['url'] == '/login') {
+          this.displayHeader = false;
+        } else {
+          this.displayHeader = true;
+        }
+      }
+    });
+
+
+
   }
   onPanel(test:any){
     this.panelId=test;
